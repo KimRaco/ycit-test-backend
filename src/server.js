dffd1305535d08ac55d7142846e45d200fccc17e
Kim Ramirez <kim.rc.1066@gmail.com>
2023-11-26 23:51:07 -0600

    cors added

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import express from  'express';
 import routerEmployees from './routers/employees.router.js';
+import cors from 'cors'
 
 
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 
 server.get('/', (req, res) => res.json('YoContigo Test server'));
 server.use('/employees', routerEmployees);
